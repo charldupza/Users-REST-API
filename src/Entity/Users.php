2a4f93d27cc64b4dd1d3711d3b40254371db92bb
charldupza <twinduct404@gmail.com>
2020-02-22 13:49:22 +0200

    Inital Commit

---
+++ src/Entity/Users.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * Username
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user;
+
+    /**
+     * Post Title
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $post;
+
+    /**
+     * User Comment
+     *
+     * @ORM\Column(type="text")
+     */
+    private $comment;
+
+    /**
+     * Post Category. Limited to one catergoy listing
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?string
+    {
+        return $this->user;
+    }
+
+    public function setUser(string $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getPost(): ?string
+    {
+        return $this->post;
+    }
+
+    public function setPost(string $post): self
+    {
+        $this->post = $post;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
