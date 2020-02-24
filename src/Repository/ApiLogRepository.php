2a7e3de41105767a196f6c7e771807e3b57f770f
charldupza <twinduct404@gmail.com>
2020-02-24 20:14:16 +0200

    Added Write To File

--- src/Repository/ApiLogRepository.php
+++ src/Repository/ApiLogRepository.php
@@ -4,6 +4,10 @@ namespace App\Repository;
 
 use App\Entity\ApiLog;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class ApiLogRepository implements ApiLogRepositoryInterface
 {
@@ -17,6 +21,15 @@ class ApiLogRepository implements ApiLogRepositoryInterface
     {
         $this->entityManager->persist($apiLog);
         $this->entityManager->flush();
+        $this->saveToFile($apiLog);
+    }
+    public function saveToFile(ApiLog $apiLog): void
+    {
+        $encoders = [new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+        $filesystem = new Filesystem();
+        $filesystem->appendToFile('logs.txt', $serializer->serialize($apiLog, 'json'));
     }
 
 }
